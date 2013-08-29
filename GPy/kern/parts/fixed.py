2703408ead80fc19a2881d19c7235296bf8e01e5
Alan Saul <alan.daniel.saul@gmail.com>
2013-08-29 13:56:41 +0100

    Added fix for fixed kern, not tested yet

--- GPy/kern/parts/fixed.py
+++ GPy/kern/parts/fixed.py
@@ -3,39 +3,48 @@
 
 from kernpart import Kernpart
 import numpy as np
+import hashlib
 
 class Fixed(Kernpart):
-    def __init__(self, input_dim, K, variance=1.):
+    def __init__(self,D,X,K,variance=1.):
         """
-        :param input_dim: the number of input dimensions
-        :type input_dim: int
+        :param D: the number of input dimensions
+        :type D: int
         :param variance: the variance of the kernel
         :type variance: float
         """
-        self.input_dim = input_dim
+        self.D = D
         self.fixed_K = K
         self.num_params = 1
         self.name = 'fixed'
         self._set_params(np.array([variance]).flatten())
+        self._X = X
+        assert self._X.shape[0] is self.fixed_K.shape[0]
 
     def _get_params(self):
         return self.variance
 
-    def _set_params(self, x):
-        assert x.shape == (1,)
+    def _set_params(self,x):
+        assert x.shape==(1,)
         self.variance = x
 
     def _get_param_names(self):
         return ['variance']
 
-    def K(self, X, X2, target):
-        target += self.variance * self.fixed_K
+    def K(self,X,X2,target):
+        if np.all(X == self._X) and X2 is None:
+            target += self.variance * self.fixed_K
 
-    def dK_dtheta(self, partial, X, X2, target):
-        target += (partial * self.fixed_K).sum()
+    def Kdiag(self,X,target):
+        if np.all(X == self._X):
+            target += self.variance * np.diag(self.fixed_K)
 
-    def dK_dX(self, partial, X, X2, target):
+    def dK_dtheta(self,partial,X,X2,target):
+        if np.all(X == self._X):# and X2 is None:
+            target += (partial * self.fixed_K).sum()
+
+    def dK_dX(self, partial,X, X2, target):
         pass
 
-    def dKdiag_dX(self, partial, X, target):
+    def dKdiag_dX(self,partial,X,target):
         pass
