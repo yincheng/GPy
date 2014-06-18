bbbe33a005bd35b3f9b40b568885682682036a3f
mzwiessele <ibinbei@gmail.com>
2014-06-18 08:40:49 -0700

    [windows] bug with ctrl-c, still not fully there yet

--- GPy/__init__.py
+++ GPy/__init__.py
@@ -20,3 +20,21 @@ import plotting
 @nottest
 def tests():
     Tester(testing).test(verbose=10)
+
+import os
+if os.name == 'nt':
+    """
+    Fortran seems to like to intercept keyboard interrupts on windows.
+    This means that when a model is optimizing and the user presses Ctrl-C,
+    the program will crash. Since it's kind of nice to be able to stop
+    the optimization at any time, we define our own handler below.
+
+    """
+    import win32api
+    import thread
+
+    def handler(sig, hook=thread.interrupt_main):
+        hook()
+        return 1
+
+    win32api.SetConsoleCtrlHandler(handler, 1)
