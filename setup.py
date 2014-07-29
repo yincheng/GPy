f4bbae90011b8ee682ffafd4afa1bc0b83af63f2
Alan Saul <alan.daniel.saul@gmail.com>
2014-07-29 12:02:47 +0100

    Added forced extraction of eggs (as we have a fair few non-py files and use the directory structure) added some files to MANIFEST and setup.py's package_data so its included upon distributing
    Conflicts:
    	setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,16 @@ import os
 import numpy
 from setuptools import setup, Extension
 from Cython.Distutils import build_ext
+from setuptools import setup
 
 # Version number
 version = '0.4.6'
 
+from pkg_resources import Requirement, resource_string
+
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    return resource_string(Requirement.parse("GPy"),fname)
+    #return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 openmp_flag = '-fopenmp'
 compile_flags = ["-march=native"]
@@ -49,17 +53,15 @@ setup(name = 'GPy',
       url="http://sheffieldml.github.com/GPy/",
       packages = ["GPy.models", "GPy.inference.optimization", "GPy.inference", "GPy.inference.latent_function_inference", "GPy.likelihoods", "GPy.mappings", "GPy.examples", "GPy.core.parameterization", "GPy.core", "GPy.testing", "GPy", "GPy.util", "GPy.kern", "GPy.kern._src.psi_comp", "GPy.kern._src", "GPy.plotting.matplot_dep.latent_space_visualizations.controllers", "GPy.plotting.matplot_dep.latent_space_visualizations", "GPy.plotting.matplot_dep", "GPy.plotting"],
       package_dir={'GPy': 'GPy'},
-      package_data={'GPy': ['GPy/examples', 'gpy_config.cfg', 'util/data_resources.json']},
+      package_data={'GPy': ['GPy/examples', 'gpy_config.cfg', 'defaults.cfg', 'installation.cfg', 'util/data_resources.json', 'util/football_teams.json']},
       py_modules=['GPy.__init__'],
       long_description=read('README.md'),
       install_requires=['numpy>=1.8', 'scipy>=0.12','matplotlib>=1.2', 'nose'],
       extras_require={
         'docs':['Sphinx', 'ipython'],
       },
-      classifiers=[
-      "License :: OSI Approved :: BSD License"],
+      classifiers=["License :: OSI Approved :: BSD License"],
       cmdclass={'build_ext': build_ext, 'inplace': True},
-      ext_modules=ext_modules
-      #ext_modules =  [Extension(name = 'GPy.kern.lfmUpsilonf2py',
-      #          sources = ['GPy/kern/src/lfmUpsilonf2py.f90'])],
+      ext_modules=ext_modules,
+      zip_safe = False
       )
